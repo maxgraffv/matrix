26167732ee10c9ae571366e495522d1b3a91c1c3
Max Graff <maksymilian.graff@gmail.com>
2024-10-20 12:51:14 +0200

    Declared Matrix struct and create, add, multiply, gaussianElimination functions

---
+++ include/matrix.h
@@ -0,0 +1,18 @@
+typedef struct 
+{
+    int columns;
+    int rows;
+    int fullLength;
+
+    int* matrixPtr;
+
+}Matrix;
+
+Matrix* createMatrix(int columns, int rows);
+
+Matrix* addMatrices(Matrix* a, Matrix* b);
+Matrix* multiplyMatrices(Matrix* a, Matrix* b);
+
+void gaussianElimination(Matrix* a);
+
+
